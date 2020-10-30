3bbfcff7c88b15105e8fee2954d55f900c2cf881
Aleena <thumbaleena@outlook.com>
2020-10-29 20:06:26 -0400

    Add files via upload

---
+++ LoopDaddy/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace LoopDaddy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //EXERCISE 3 -
+            //Use a for loop to output a triangle of asterisks with a height of ten.
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+
+            //EXERCISE 2 - 
+            //Prompt the user for a number.  Use a for loop to output all the numbers from that number to 0.           
+            /*Console.Write("Please enter a number: ");
+            string userInput = Console.ReadLine();
+            int number;
+            number = int.Parse(userInput);
+            for (int i = number; i>= 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+           */
+
+
+            //EXERCISE 1 - 
+            //Use a do-while loop to output "Hello, World!" in a loop.  Each time you output "Hello, World!"
+            //Ask the user wheher or not they wouldl ike to continue.
+            /* string Answer;
+            do
+            {
+                Console.Write("Are you ready for this jelly?(yes/no) ");
+                 Answer = Console.ReadLine();
+                Console.WriteLine("Hello World!");
+            }
+            while (Answer == "yes"); */
+        }
+    }
+}
